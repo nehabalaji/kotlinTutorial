beb8c4b14d6a579fc8cecca3182ac076969f48cd
nehabalaji <nehabalaji10@gmail.com>
2020-08-16 22:27:34 +0530

    add a string

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,5 +1,13 @@
 fun main(){
     println("Hello world!")
     var myVariable: Int = 10
+    var anotherVariable = 5
     println(myVariable)
+    myVariable=1
+    println("The value of the variable is: $myVariable")
+    println()
+    println("The value of another variable is: $anotherVariable")
+    println("The sum of the two variables are ${myVariable+anotherVariable}")
+    val s = "This is a string!"
+    println("The given string is: $s")
 }
