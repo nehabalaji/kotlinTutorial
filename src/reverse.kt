12490e43bad5406a34ee7ab48a0a42dbeea3cd16
nehabalaji <nehabalaji10@gmail.com>
2020-09-01 15:02:28 +0530

    add lists

--- src/reverse.kt
+++ src/reverse.kt
@@ -1,13 +1,13 @@
 fun main(){
     val list = mutableListOf<Int>()
     println("Enter 5 numbers")
-    for (i in list){
+    for (i in 0..4){
         val n = readLine()?.toInt()
         if (n!=null)
         list.add(n)
     }
     println("The numbers in reverse order are : ")
-    for (i in list[4] downTo list[0]){
-        println(i)
+    for (i in 4 downTo 0){
+        println(list[i])
     }
 }
